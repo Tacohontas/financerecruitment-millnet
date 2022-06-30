c0c24a32fe0227a685f3f5dc4994799637376b49
Jonathan Undhagen <Jonathan.undhagen@gmail.com>
2022-06-30 16:41:53 +0200

    add version number and yarn

--- financerecruitment-millnet.php
+++ financerecruitment-millnet.php
@@ -39,7 +39,7 @@ if ( ! class_exists( 'Financerecruitment_Millnet\\Plugin' ) ) :
 		/**
 		 * Version of plugin
 		 */
-		const VERSION = '3.7.0';
+		const VERSION = '1.0.0';
 
 		/**
 		 * @var string
