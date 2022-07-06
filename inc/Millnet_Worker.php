9f3e624f6cd0a2efe3884f95f8c9877453ebd71c
Jonathan Undhagen <Jonathan.undhagen@gmail.com>
2022-07-06 15:59:21 +0200

    add method for populating fields

--- inc/Millnet_Worker.php
+++ inc/Millnet_Worker.php
@@ -41,7 +41,39 @@ class Millnet_Worker {
 
 		return $form;
 	}
-		
+
+	/**
+	 * Populate custom millnet fields
+	 *
+	 * @param array $form
+	 * @return array
+	 */
+	public function populate_fields( $form ) {
+		$client = gen()->millnet_soap();
+		$login_success = $client->login();
+
+		if ( ! $login_success ) {
+			return $form;
+		}
+
+		$groups = $client->get_groups();
+
+		foreach ( $form['fields'] as &$field ) {
+			if ( strpos( $field->cssClass, 'fr-millnet-user-group' ) !== false ) {
+				$this->populate_field_with_group( 'user_types', $groups, $field );
+			}
+
+			if ( strpos( $field->cssClass, 'fr-millnet-fraa-frtemp' ) !== false ) {
+				$this->populate_field_with_group( 'fraa_frtemp', $groups, $field );
+			}
+
+			if ( strpos( $field->cssClass, 'fr-millnet-salary-type' ) !== false ) {
+				$this->populate_field_with_group( 'salary_type', $groups, $field );
+			}
+		}
+
+		return $form;
+	}
 	}
 
 }
